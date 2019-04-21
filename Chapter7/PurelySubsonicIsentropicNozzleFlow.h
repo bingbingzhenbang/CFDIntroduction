36b50d45bb06fc2d63b6cf35596aeb64f93a3ef4
957438119 <957438119@qq.com>
2019-04-21 22:42:02 +0800

    1

--- Chapter7/PurelySubsonicIsentropicNozzleFlow.h
+++ Chapter7/PurelySubsonicIsentropicNozzleFlow.h
@@ -2,5 +2,15 @@
 
 class PurelySubsonic
 {
-
+public:
+    double GetA(double x)
+    {
+        double tmp = x - 1.5;
+        if (0 <= x && x <= 1.5)
+            return 1 + 2.2 * tmp * tmp;
+        else if (x > 1.5 && x <= 3.0)
+            return 1 + 0.2223 * tmp * tmp;
+        else
+            return 0.0;
+    }
 };
